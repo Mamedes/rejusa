99f3ca3951fcbe0c1d2e28b2eeb9ef5baf75d007
Mamedes <mamedeser@gmail.com>
2021-01-04 14:23:00 -0400

    New  Arquitetura

---
+++ config.py
@@ -0,0 +1,41 @@
+
+import os
+from   decouple import config
+
+class Config(object):
+
+
+
+    # Set up the App SECRET_KEY
+    SECRET_KEY = config('SECRET_KEY', default='Rejusa_S#perS3crEt_007')
+
+    # This will create a file in <app> FOLDER
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/rejusa'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+    # Security
+    SESSION_COOKIE_HTTPONLY  = True
+    REMEMBER_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_DURATION = 3600
+
+    # PostgreSQL database
+    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
+        config( 'DB_ENGINE'   , default='postgresql'    ),
+        config( 'DB_USERNAME' , default='postgres'       ),
+        config( 'DB_PASS'     , default='postgres'          ),
+        config( 'DB_HOST'     , default='localhost'     ),
+        config( 'DB_PORT'     , default=5432            ),
+        config( 'DB_NAME'     , default='rejusa' )
+    )
+
+class DebugConfig(Config):
+    DEBUG = True
+
+# Load all possible configurations
+config_dict = {
+    'Production': ProductionConfig,
+    'Debug'     : DebugConfig
+}
